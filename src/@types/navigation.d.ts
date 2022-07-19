625686acfec6881cfcf844bb2cac8b14f38cd960
leords <leo_ro.drigues@hotmail.com>
2022-07-19 20:06:05 -0300

    Aula 2

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global { //declaração global
+    namespace ReactNavigation { 
+        interface RootParamList {
+            home: undefined
+            new: undefined
+            details: {orderId: string}
+        }
+    }
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,97 @@
+import { Heading, HStack, IconButton, Text, useSafeArea, useTheme, VStack, FlatList, Center } from 'native-base';
+import { ItemClick } from 'native-base/lib/typescript/components/composites/Typeahead/useTypeahead/types';
+import { ChatTeardropText, SignOut } from 'phosphor-react-native';
+import { useState } from 'react';
+
+import Logo from '../assets/logo_secondary.svg';
+import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
+import { Button } from '../components/Button';
+import { useNavigation } from '@react-navigation/native';
+
+export function Home() {
+
+    const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+    const [orders, setOrders] = useState<OrderProps[]>([{
+        id: '123',
+        patrimony: '123456',
+        when: '18/07/2022 as 10:00',
+        status: 'open'
+    }]);
+
+    const navigation = useNavigation();
+    const { colors } = useTheme();
+
+    function handleNewOrder() {
+        navigation.navigate('new')
+    }
+
+    function handleOpenDetais(orderId: string) {
+        navigation.navigate('details', {orderId} )
+    }
+
+    return (
+        <VStack flex={1} pb={6} bg="gray.700">
+            <HStack
+                w="full"
+                justifyContent="space-between"
+                alignItems="center"
+                bg="gray.600"
+                pt={12}
+                pb={5}
+                px={6}
+            >
+                <Logo />
+                <IconButton 
+                    icon={<SignOut size={26} color={colors.gray[300]}/>}           
+                />
+            </HStack>
+
+        <VStack flex={1} px={6}>
+                <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
+                    <Heading color="gray.200">
+                        Meus Chamados
+                    </Heading>
+                    <Text color="gray.200">
+                        4
+                    </Text>
+                </HStack>
+
+                <HStack space={3} mb={8} >
+                    <Filter 
+                        type="open"
+                        title='em andamento'
+                        onPress={() => setStatusSelected('open')}
+                        isActive={statusSelected === 'open'}
+                    />
+                    <Filter 
+                        type="closed"
+                        title='finalizados'
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'} //retorna true se a comparação estiver certa! 
+                    />
+                </HStack>
+
+                <FlatList 
+                    data={orders}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetais(item.id)} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{paddingBottom: 100}}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40}/> 
+                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                            </Text>
+                        </Center>
+                    )}
+                />
+
+                <Button title='Nova Solicitação' onPress={handleNewOrder}/>
+
+            </VStack>
+        </VStack>
+  );
+}
